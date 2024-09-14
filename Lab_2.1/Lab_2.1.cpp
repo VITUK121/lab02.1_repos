5cbbe13bc7cd777a0f3dd4d46a3e47b0e25ec06b
unknown <fagefd@gmail.com>
2024-09-14 21:02:01 +0300

    first commit lab_02.1

--- Lab_2.1/Lab_2.1.cpp
+++ Lab_2.1/Lab_2.1.cpp
@@ -14,4 +14,6 @@ int main()
     double z1 = pow(cos(x), 4) + pow(sin(y), 2) + 0.25*pow(sin(2*x), 2) - 1;
     double z2 = sin(y + x) * sin(y - x);
     cout << "z1 = " << z1 << "\t" << "z2 = " << z2 << endl;
+
+    system("pause");
 }
